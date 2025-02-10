1a94a6f9461186f4869b985822f0187f91967e80
Andrew M. Pankratz <apankra@wgu.edu>
2025-02-10 18:27:54 -0500

    Fix errorhandler bug

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -1,9 +1,20 @@
 import { NextFunction, Response, Request } from "express";
 import { CustomError } from "../_helpers/errors-helper";
 
-export const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
-    res.status(err.getCode())
-        .json({message: err.message, data: err.getData()});
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    let code: number,
+        data: any;
+    
+    if(err instanceof CustomError){
+        code = (err as CustomError).getCode();
+        data = (err as CustomError).getData();
+    }else{
+        code = 500,
+        data = {}
+    }
+    
+    res.status(code)
+        .json({message: err.message, data });
 }
 
 export function error404(req: Request, res: Response, next: NextFunction) {
