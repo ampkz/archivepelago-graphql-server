3be28e1783bd3929c6d8e8325bb3b63bbd0af237
Andrew M. Pankratz <apankra@wgu.edu>
2025-02-11 08:30:31 -0500

    Create createUser mutation

--- src/graphql/typeDefs/userType.ts
+++ src/graphql/typeDefs/userType.ts
@@ -10,4 +10,16 @@ type User {
 type Query { 
     user(email: String!): User
 }
+
+type Mutation {
+    createUser(input: CreateUserInput): User
+}
+
+input CreateUserInput {
+    email: String!
+    firstName: String!
+    lastName: String!
+    secondName: String
+    auth: String!
+}
 `
