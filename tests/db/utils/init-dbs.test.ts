ac490107322604632e6d56bf5ebab49c6a7fd25d
Andrew M. Pankratz <apankra@wgu.edu>
2025-02-13 09:18:20 -0500

    Add tests for duplicate DB creation

--- tests/db/utils/init-dbs.test.ts
+++ tests/db/utils/init-dbs.test.ts
@@ -2,6 +2,7 @@ import { initializeDBs, destroyTestingDBs, verifyDB, ErrorMsgs as DBInitErrorMsg
 import { getSessionOptions } from '../../../src/_helpers/db-helper';
 import dotenv from 'dotenv';
 import { InternalError } from '../../../src/_helpers/errors-helper';
+import neo4j, { Session, Driver, Record } from 'neo4j-driver';
 
 dotenv.config();
 
@@ -16,9 +17,63 @@ describe(`DB initializing tests`, () => {
         await expect(verifyDB(getSessionOptions(process.env.USERS_DB as string).database)).resolves.toBeFalsy();
     });
 
-    it(`should not create a duplicate DB`, async () => {
+    it(`should not create a duplicate Users DB`, async () => {
+        const mockUsersRecord = {
+            get: (key: any) => {
+                if (key === 'address') {
+                return ``;
+                }
+                return {properties: {}};
+            }} as unknown as Record;
+        
+        const driverMock = {
+            session: jest.fn().mockReturnValue({
+                run: jest.fn().mockResolvedValue({records: [mockUsersRecord]}),
+                close: jest.fn(),
+            } as unknown as Session),
+            close: jest.fn(),
+            getServerInfo: jest.fn()
+        } as unknown as Driver;
+        
+        const driverSpy = jest.spyOn(neo4j, "driver");
+        driverSpy.mockReturnValueOnce(driverMock);
+        
         await expect(initializeDBs()).rejects.toThrow(DBInitErrorMsgs.COULD_NOT_CREATE_DB);
     });
+
+    it(`should not create a duplicate Archive DB`, async () => {
+        const mockUsersRecord = {
+            get: (key: any) => {
+                if (key === 'address') {
+                    return `${process.env.NEO4J_HOST}:${process.env.NEO4J_PORT}`;
+                }
+            },
+            } as unknown as Record;
+
+            const mockArchiveRecord = {
+            get: (key: any) => {
+                if (key === 'address') {
+                    return ``;
+                }
+            },
+            } as unknown as Record;
+        
+        const driverMock = {
+            session: jest.fn().mockReturnValue({
+                run: jest.fn().mockResolvedValueOnce({records: [mockUsersRecord]})
+                    .mockResolvedValueOnce({records: [mockArchiveRecord]}),
+                close: jest.fn(),
+            } as unknown as Session),
+            close: jest.fn(),
+            getServerInfo: jest.fn()
+        } as unknown as Driver;
+        
+        const driverSpy = jest.spyOn(neo4j, "driver");
+        driverSpy.mockReturnValueOnce(driverMock);
+        
+        await expect(initializeDBs()).rejects.toThrow(DBInitErrorMsgs.COULD_NOT_CREATE_DB);
+    });
+    
 
     it(`should have created a Users DB`, async () => {
         await expect(verifyDB(getSessionOptions(process.env.USERS_DB as string).database)).resolves.toBeTruthy();
