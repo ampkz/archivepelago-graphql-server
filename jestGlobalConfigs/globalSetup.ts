6d7cd807bae4844172b4b51506e0b2aaa9fa9d55
Andrew M. Pankratz <apankra@wgu.edu>
2025-02-26 12:47:54 -0500

    Housekeeping

--- jestGlobalConfigs/globalSetup.ts
+++ jestGlobalConfigs/globalSetup.ts
@@ -1,5 +1,5 @@
-import { initializeDBs } from '../src/db/utils/init-dbs';
 import dotenv from 'dotenv';
+import { initializeDBs } from '../src/db/utils/init-dbs';
 
 module.exports = async () => {
 	dotenv.config();
