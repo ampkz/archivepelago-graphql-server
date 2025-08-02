33e98dd143ec59e924be4fef7a321f4e510741ea
Andrew M. Pankratz <apankra@wgu.edu>
2025-08-02 16:09:39 -0400

    Start migration to use auth-neo4j

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,25 +4,28 @@ import { expressMiddleware } from '@as-integrations/express5';
 import express, { Express } from 'express';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { error404, errorHandler } from '../middleware/errors';
-import authenticateRouter from '../routing/auth/authenticate';
+// import authenticateRouter from '../routing/auth/authenticate';
 import cookieParser from 'cookie-parser';
 import http from 'http';
 import cors from 'cors';
-import { AuthorizedUser } from '../auth/authorization';
-import userType from '../graphql/typeDefs/userType';
-import userResolver from '../graphql/resolvers/userResolver';
+// import { AuthorizedUser } from '../auth/authorization';
+// import userType from '../graphql/typeDefs/userType';
+// import userResolver from '../graphql/resolvers/userResolver';
 import personType from '../graphql/typeDefs/personType';
 import personResolver from '../graphql/resolvers/personResolver';
 import labelType from '../graphql/typeDefs/labelType';
 import labelResolver from '../graphql/resolvers/labelResolver';
 import correspondenceType from '../graphql/typeDefs/correspondenceType';
 import correspondenceResolver from '../graphql/resolvers/correspondenceResolver';
-import { validateSessionToken } from '../auth/session';
-import { verifyToken } from '../_helpers/auth-helpers';
+// import { validateSessionToken } from '../auth/session';
+// import { verifyToken } from '../_helpers/auth-helpers';
 import helmet from 'helmet';
+import authNeo4j from '@ampkz/auth-neo4j';
+import { validateSessionToken } from '@ampkz/auth-neo4j/dist/sessions/crud-session';
+import { User } from '@ampkz/auth-neo4j/dist/users/user';
 
 interface MyContext {
-	authorizedUser?: AuthorizedUser | null;
+	authorizedUser?: User | null;
 }
 
 async function startServer() {
@@ -30,8 +33,8 @@ async function startServer() {
 
 	const httpServer = http.createServer(app);
 
-	const typeDefs = mergeTypeDefs([userType, personType, labelType, correspondenceType]);
-	const resolvers = mergeResolvers([userResolver, personResolver, labelResolver, correspondenceResolver]);
+	const typeDefs = mergeTypeDefs([personType, labelType, correspondenceType]);
+	const resolvers = mergeResolvers([personResolver, labelResolver, correspondenceResolver]);
 
 	/* istanbul ignore next line */
 	const server = new ApolloServer<MyContext>({
@@ -62,15 +65,16 @@ async function startServer() {
 		'/graphql',
 		expressMiddleware(server, {
 			context: async ({ req }) => {
-				const jwt = req.cookies.jwt;
-				const { authToken } = verifyToken(jwt);
-				const svr = await validateSessionToken(authToken);
+				const token = req.cookies.token;
+				const svr = await validateSessionToken(token);
 				return { authorizedUser: svr.user };
 			},
 		})
 	);
 
-	app.use(authenticateRouter);
+	// app.use(authenticateRouter);
+
+	app.use(authNeo4j());
 
 	app.use(error404);
 
